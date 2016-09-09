f9d087cd6277dd809e8545be48789ca706664cc8
zowbman <zhangweibao@kugou.net>
2016-09-09 19:45:28 +0800

    新增：1、ads测试项目

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ bash gradlew clean deploy || exit $?
 #将编译后文件同步到outer目录
 mkdir -p outer/lib
 rsync -aq --delete build/deploy/ outer/lib/
-rsync -aq --delete build/deploy/webapp outer/lib/webapp/
+rsync -aq --delete build/deploy/webapp/ outer/lib/webapp/
 
 #将配置文件同步到outer/etc目录
 mkdir -p outer/etc
